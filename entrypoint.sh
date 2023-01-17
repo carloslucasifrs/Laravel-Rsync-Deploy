af15f5ea4b5b2c93052c36b89735c3b432d320a9
carloslucasifrs <71910449+carloslucasifrs@users.noreply.github.com>
2023-01-17 19:48:42 -0300

    add Optimizing Configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,7 @@ SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
 
 # Run Rsync synchronization
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
+sh -c "php artisan config:cache"
+sh -c "php artisan config:cache"
+sh -c "php artisan view:cache"
+
