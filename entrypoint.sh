0fd6b65645271a8e4ee46964a76fcdde9bfeb89d
carloslucasifrs <71910449+carloslucasifrs@users.noreply.github.com>
2023-01-17 19:56:45 -0300

    add sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
 
 # Run Rsync synchronization
-sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
-sh -c "php artisan config:cache"
-sh -c "php artisan config:cache"
-sh -c "php artisan view:cache"
+sh -c "sudo rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
+sh -c "sudo php artisan config:cache"
+sh -c "sudo php artisan config:cache"
+sh -c "sudo php artisan view:cache"
 sh -c "sudo chown www-data:www-data -R /var/www/carloseduardo.tech"
 
 
